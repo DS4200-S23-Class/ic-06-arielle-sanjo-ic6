b48fd384d8596e3e4fa33226a7c351fcd953df57
Arielle <arielle@MacBook-Pro-92.local>
2023-02-13 15:20:20 -0500

    changes 02/13

---
+++ main.js
@@ -0,0 +1,18 @@
+let gar1=document.getElementById("gar1");
+
+
+// dynamic styling
+function submit2Clicked() {
+    let maxPt = document.getElementById("max");
+
+    maxPt.classList.add("red"); 
+
+}
+
+document.getElementById("subButton2")
+        .addEventListener("click", submit2Clicked);
+        
+
+
+
+
